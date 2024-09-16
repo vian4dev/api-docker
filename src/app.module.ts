3d2ca7f3079867f590fee25b1e8a5bc72f4e251a
Gabriel Viana <viannadoze@gmail.com>
2024-09-15 21:29:37 -0300

    Update README.md

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql:3300',
+      port: 3300,
+      username: 'admin',
+      password: 'root',
+      database: 'rocketseat-db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
